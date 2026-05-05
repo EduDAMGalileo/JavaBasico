383c175a0682bdb61257f0fcd0be128e742d8de1
EduDAMGalileo <eduardoj.niegar@educa.jcyl.es>
2026-05-05 15:35:30 +0200

    Intro a las colecciones

---
+++ src/colecciones/RegistroClientes.java
@@ -0,0 +1,34 @@
+package colecciones;
+
+public class RegistroClientes {
+    // MALA PRÁCTICA, PARA ILUSTRAR EL PROBLEMA (Enfoque manual): 
+    private Cliente[] clientes;
+    private int cantidadActual;
+
+    public RegistroClientes() {
+        this.clientes = new Cliente[5]; 
+        this.cantidadActual = 0;
+    }
+
+    public void registrar(Cliente nuevoCliente) {
+        // Ensuciamos nuestra lógica de negocio para vigilar constantemente la memoria
+        if (cantidadActual == clientes.length) {
+            System.out.println("Array lleno. Ejecutando costoso redimensionamiento manual...");
+            
+            // Instanciamos un array nuevo, por ejemplo del doble de tamaño
+            Cliente[] nuevoArray = new Cliente[clientes.length * 2];
+            
+            // Mudamos a los inquilinos del edificio viejo al nuevo (operación muy costosa en CPU)
+            for (int i = 0; i < clientes.length; i++) {
+                nuevoArray[i] = clientes[i];
+            }
+            
+            // Reemplazamos la referencia. Dejamos que el Garbage Collector destruya el array viejo.
+            clientes = nuevoArray; 
+        }
+        
+        // Ahora sí, por fin, insertamos el dato que nos importaba
+        clientes[cantidadActual] = nuevoCliente;
+        cantidadActual++;
+    }
+}
